b5055c2635a0d403e091cf02082f06a52aa8c9cb
1leksi <151362814+1leksi@users.noreply.github.com>
2023-12-23 09:46:03 +0300

    Add files via upload

---
+++ kyrsa4.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include "Header.h"
+#include "Source1.cpp"
+#include "Source2.cpp"
+#include "Source3.cpp"
+
+using namespace std;
+
+int main() {
+
+    setlocale(0, "");
+    int laba;
+    cout << '\n';
+    cout << "enter de number of de laboratory work (if you want to exit de program or laboratory work, enter 0): ";
+    cin >> laba;
+    cout << '\n';
+    switch (laba) {
+    case 1:
+        main1();
+        break;
+    case 2:
+        main2();
+        break;
+    case 3:
+        main3();
+        break;
+    case 0:
+        exit(0);
+    default:
+        cout << "there is no such laboratory work" << '\n';
+        break;
+    }
+    return 0;
+}
